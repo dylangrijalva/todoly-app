c495c232c3bb862d098317ced02a03b5b5923bbe
Dylan Grijalva <dagndev@gmail.com>
2021-09-23 17:56:04 -0600

    [feat]: Add endpoints for basic CRUD operations on a task

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -1,7 +1,154 @@
 const router = require('express').Router();
+const verifyJwt = require('../middlewares/verifyJwt');
+const { generateId, toBoolean } = require('../utils');
+const { PrismaClient } = require('@prisma/client');
+const db = new PrismaClient();
 
-router.get('/', (req, res) => {
-  return res.status(200).send('Tasks Endpoint📙!');
+router.use(verifyJwt);
+
+router.get('/', async (req, res) => {
+  const { sub: userId } = req.payload;
+
+  const tasks = await db.task.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+      content: true,
+      isCompleted: true,
+    },
+  });
+
+  return res.status(200).json(tasks);
+});
+
+router.post('/', async (req, res) => {
+  if (!req.body.content || req.body.content === '') {
+    return res.status(400).json({
+      status: 400,
+      message: 'Content cannot be null or empty',
+    });
+  }
+
+  if (!req.body.isCompleted) {
+    return res.status(400).json({
+      status: 400,
+      message: 'IsCompleted cannot be null',
+    });
+  }
+
+  let { content, isCompleted } = req.body;
+  const { sub: userId } = req.payload;
+
+  isCompleted = toBoolean(isCompleted);
+
+  const newTask = await db.task.create({
+    data: {
+      id: await generateId(),
+      content: content,
+      isCompleted: isCompleted,
+      userId: userId,
+    },
+  });
+
+  return res.status(201).json(newTask);
+});
+
+router.put('/:id', async (req, res) => {
+  if (!req.params.id || req.params.id === '') {
+    return res.status(400).json({
+      status: 400,
+      message: 'You must provide a valid id',
+    });
+  }
+
+  if (!req.body.content || req.body.content === '') {
+    return res.status(400).json({
+      status: 400,
+      message: 'Content cannot be null or empty',
+    });
+  }
+
+  if (!req.body.isCompleted) {
+    return res.status(400).json({
+      status: 400,
+      message: 'IsCompleted cannot be null',
+    });
+  }
+
+  const taskId = req.params.id;
+  let { content, isCompleted } = req.body;
+
+  isCompleted = toBoolean(isCompleted);
+
+  const existingTask = await db.task.findFirst({
+    select: {
+      id: true,
+    },
+    where: {
+      id: taskId,
+    },
+  });
+
+  if (!existingTask) {
+    return res.status(404).json({
+      status: 404,
+      message: 'The requested task does not exist',
+    });
+  }
+
+  const updatedTask = await db.task.update({
+    where: {
+      id: taskId,
+    },
+    data: {
+      content: content,
+      isCompleted: isCompleted,
+    },
+    select: {
+      id: true,
+      content: true,
+      isCompleted: true,
+    },
+  });
+
+  return res.status(200).json(updatedTask);
+});
+
+router.delete('/:id', async (req, res) => {
+  if (!req.params.id || req.params.id === '') {
+    return res.status(400).json({
+      status: 400,
+      message: 'You must provide a valid id',
+    });
+  }
+
+  const taskId = req.params.id;
+
+  const existingTask = await db.task.findFirst({
+    select: {
+      id: true,
+    },
+    where: {
+      id: taskId,
+    },
+  });
+
+  if (!existingTask) {
+    return res.status(404).json({
+      status: 404,
+      message: 'The requested task does not exist',
+    });
+  }
+
+  await db.task.delete({
+    where: {
+      id: taskId,
+    },
+  });
+
+  return res.status(204).send();
 });
 
 module.exports = router;
