528f45f71b4c3aa547a80c17e863751c3a1134d8
Dylan Grijalva <dagndev@gmail.com>
2021-09-24 05:27:56 -0600

    Ensure .env file has been already created previously before running the app

--- backend/run.sh
+++ backend/run.sh
@@ -1,2 +1,6 @@
-docker-compose up
-docker exec -it api npx prisma migrate dev
+if [ ! -f ".env" ]; then
+    echo 'Could not be able to start the app due .env file is missing'
+else
+    docker-compose up
+    docker exec -it api npx prisma migrate dev  
+fi
